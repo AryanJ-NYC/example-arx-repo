775323bea7c8b67d228bd904ac0b3446d43bcee1
Aryan Jabbari <AryanJabbari@gmail.com>
2023-02-07 22:25:29 -0500

    commit

--- README.md
+++ README.md
@@ -1,38 +1,10 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Arx Chip Example Repo
 
-## Getting Started
+A couple of questions here:
 
-First, run the development server:
+1. Am I getting the address correctly?
+2. Every time I ask for a signature, I get a different one. Why is that?
+3. Which message should I be throwing into the validator: the hex or "some message"?
+4. Why do we need to hash the message before sending it to the chip? Would it be enough to just hex it?
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+I use https://www.verifybitcoinmessage.com to try and validate the signature.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,174 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+'use client';
+import base64url from 'base64url';
+import * as bitcoin from 'bitcoinjs-lib';
+import { derToJose } from 'ecdsa-sig-formatter';
+import { sha256, toUtf8Bytes } from 'ethers';
+import { useState } from 'react';
+import * as u8a from 'uint8arrays';
 
 export default function Home() {
+  const [address, setAddress] = useState('');
+  const [signature, setSignature] = useState('');
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex flex-col h-full items-center gap-y-16 px-4 py-16 justify-between">
+      <p>{address ? `Address: ${address}` : ''}</p>
+      <p className="break-all">{signature ? `Signature: ${signature}` : ''}</p>
+      <div className="flex flex-col gap-y-8">
+        <button
+          className="bg-green-600 rounded-full text-green-50 px-6 py-2"
+          onClick={async () => {
+            try {
+              const req: any = {
+                publicKey: {
+                  allowCredentials: [
+                    { id: fromHexString('02'), transports: ['nfc'], type: 'public-key' },
+                  ],
+                  challenge: new Uint8Array([
+                    113, 241, 176, 49, 249, 113, 39, 237, 135, 170, 177, 61, 15, 14, 105, 236, 120,
+                    140, 4, 41, 65, 225, 107, 63, 214, 129, 133, 223, 169, 200, 21, 88,
+                  ]),
+                  timeout: 60000,
+                  userVerification: 'discouraged',
+                },
+              };
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
+              const xdd: any = await navigator.credentials.get(req);
+              const sig = xdd.response.signature;
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+              if (typeof sig !== 'undefined') {
+                const sss = buf2hex(sig);
+                const _keys = parseKeys(sss);
+
+                if (_keys) {
+                  console.log(_keys);
+                  setAddress(_keys);
+                }
+              }
+            } catch (err) {
+              console.log('Error with scan', err);
+            }
+          }}
         >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Scan for Address
+        </button>
+        <button
+          className="bg-green-600 rounded-full text-green-50 px-6 py-2"
+          onClick={async () => {
+            try {
+              const sigCmd = generateCmd(1, 1, 'some message');
+              const hexSigCmd = fromHexString(sigCmd);
+              console.log({ hexSigCmd, sigCmd });
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
+              const req: any = {
+                publicKey: {
+                  allowCredentials: [{ id: hexSigCmd, transports: ['nfc'], type: 'public-key' }],
+                  challenge: new Uint8Array([
+                    113, 241, 176, 49, 249, 113, 39, 237, 135, 170, 177, 61, 15, 14, 105, 236, 120,
+                    140, 4, 41, 65, 225, 107, 63, 214, 129, 133, 223, 169, 200, 21, 88,
+                  ]),
+                  timeout: 60000,
+                  userVerification: 'discouraged',
+                },
+              };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+              const xdd: any = await navigator.credentials.get(req);
+
+              const sig = xdd.response.signature;
+              const ES256 = base64url.toBase64(derToJose(Buffer.from(sig), 'ES256'));
+              setSignature(ES256);
+              console.log({ ES256 });
+
+              // the code below actually gets us the same signature as the three lines above
+              const unpackedSig = unpackDERSignature(new Uint8Array(sig));
+              const { r, s } = unpackedSig;
+              console.log({ base64RS: u8a.toString(new Uint8Array([...r, ...s]), 'base64') });
+            } catch (err) {
+              console.log('Error with scan', err);
+            }
+          }}
         >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Sign &ldquo;some message&rdquo;
+        </button>
       </div>
     </main>
-  )
+  );
+}
+
+function buf2hex(buffer: Buffer | Uint8Array) {
+  return [...new Uint8Array(buffer)].map((x) => x.toString(16).padStart(2, '0')).join('');
+}
+
+function generateCmd(cmd: number, keyslot: number, message: string) {
+  // what's the point of hashing this with sha256?
+  let messageBytes = sha256(toUtf8Bytes(message));
+  console.log({ message, messageBytes });
+
+  // Remove prepended 0x.
+  messageBytes = messageBytes.slice(2);
+
+  let cmdBytes: string | Uint8Array = new Uint8Array(2);
+  cmdBytes[0] = cmd;
+  cmdBytes[1] = keyslot;
+  cmdBytes = buf2hex(cmdBytes);
+
+  const inputBytes = cmdBytes + messageBytes;
+  return inputBytes;
+}
+
+function parseKeys(payload: string) {
+  try {
+    const primaryPublicKeyLength = parseInt('0x' + payload.slice(0, 2)) * 2;
+    const primaryPublicKeyRaw = payload.slice(2, primaryPublicKeyLength + 2);
+    const { address } = bitcoin.payments.p2pkh({ pubkey: Buffer.from(primaryPublicKeyRaw, 'hex') });
+    return address;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+
+const toHex = (b: Uint8Array) => '0x' + u8a.toString(b, 'base16');
+function unpackDERSignature(sig: Uint8Array) {
+  const bytes = sig.values();
+
+  if (bytes.next().value !== 0x30) {
+    throw Error('Invalid header, ' + toHex(sig));
+  }
+  bytes.next(); // ignore second byte of header
+
+  if (bytes.next().value !== 0x02) {
+    throw Error('Invalid header (2).');
+  }
+
+  let length_r = bytes.next().value;
+  if (length_r == 33) {
+    bytes.next(); // ignore prepended padding
+    length_r -= 1;
+  }
+  const r = new Uint8Array(length_r);
+  for (let i = 0; i < length_r; ++i) {
+    r[i] = bytes.next().value;
+  }
+
+  if (bytes.next().value !== 0x02) {
+    throw Error('Invalid header (2).');
+  }
+
+  let length_s = bytes.next().value;
+  if (length_s == 33) {
+    bytes.next(); // ignore prepended padding
+    length_s -= 1;
+  }
+  const s = new Uint8Array(length_r);
+  for (let i = 0; i < length_s; ++i) {
+    s[i] = bytes.next().value;
+  }
+
+  return { r, s };
+}
+
+function fromHexString(hexString: string) {
+  // @ts-expect-error
+  return new Uint8Array(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
 }
